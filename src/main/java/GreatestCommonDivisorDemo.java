4d5a2c1dac96054046626537d29df79c6e10c01b
ScorpionOrange <scorpionorange@126.com>
2017-01-05 00:23:10 +0800

    增加备注说明。（in 2017.1.04）

--- src/main/java/GreatestCommonDivisorDemo.java
+++ src/main/java/GreatestCommonDivisorDemo.java
@@ -7,6 +7,14 @@ public class GreatestCommonDivisorDemo {
                            + GreatestCommonDivisor(1984, 1987));
     }
 
+    /**
+     * 欧几里得算法：
+     * 1、确保被除数不为零；
+     * 2、两数相除求余数，去除大数，小数与余数相除，依次重复；
+     * @param m 大数；
+     * @param n 被除数；
+     * @return 返回最后的余数，则为最大公约数；
+     */
     public static long GreatestCommonDivisor(long m, long n){
         while (n != 0){
             long rem = m % n;
